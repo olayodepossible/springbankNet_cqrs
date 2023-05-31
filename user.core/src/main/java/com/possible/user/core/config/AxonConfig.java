ee0e212a2caade2b9779d9c2e54f6e33ca76be66
Akin <>
2023-05-31 22:50:01 +0100

    project setup

---
+++ user.core/src/main/java/com/possible/user/core/config/AxonConfig.java
@@ -0,0 +1,10 @@
+package com.possible.user.core.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AxonConfig {
+    private String mongoHost;
+    private int mongoPort;
+    private String mongoDatabase;
+}
